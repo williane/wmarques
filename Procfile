2552301c3054dc2b7c5a53efae52aab80d8ae4f4
unknown <williane.tads@gmail.com>
2020-02-23 13:54:13 -0300

    configuring app

---
+++ Procfile
@@ -0,0 +1,1 @@
+Web: gunicorn website.wsgi --log-file -
--- wassis/settings.py
+++ wassis/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'krqhous-ux_7z&%y9ncku%6&k=vngdy%v(thr)xcfs_9ft&((v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wassis.herokuapp.com']
 
 
 # Application definition
@@ -120,10 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    'static',
-]
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
