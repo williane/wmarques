61f9e5b1966efb7cd961dd58afa0acad54f54a5e
unknown <williane.tads@gmail.com>
2020-03-15 21:19:30 -0300

    Configuring the app

--- wassis/settings.py
+++ wassis/settings.py
@@ -119,7 +119,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
