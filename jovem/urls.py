090f32a2799273c812f7576003b1a038413d467f
unknown <williane.tads@gmail.com>
2020-03-21 19:10:27 -0300

    otherversion

--- jovem/urls.py
+++ jovem/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import index, profile, acompanhar_status, form_user, indicacao, form_instituicao, instituicao_list
-from .views import usuario_list, usuario_update, instituicao_update, indicado_update, send_email
+from .views import usuario_list, usuario_update, instituicao_update, indicado_update, send_email, form_resetpassword
 
 urlpatterns = [
     path('', index, name='index'),
@@ -17,4 +17,5 @@ urlpatterns = [
     path('instituicao_update/<int:id>', instituicao_update, name='instituicao_update'),
     path('indicado_update/<int:id>', indicado_update, name='indicado_update'),
     path('send_email', send_email, name='send_email'),
+    path('passwordReset', form_resetpassword, name='form_resetpassword'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jovem/views.py
+++ jovem/views.py
@@ -1,11 +1,23 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.db.models import Avg, Count, Min, Sum
 from django.core.mail import send_mail
 from .models import Instituicao, Usuario, indicado, Indicado_status
 from .forms import InstituicaoForm, UsuarioForm, IndicadoForm, IndicadoForm2
+from random import choice
 import smtplib
+
+
+def gerador_senha(tamanho):
+    caracteres = "0123456789abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%&*()_+}{`^?;:>/-+.,"
+    senha = ""
+    for i in range(tamanho):
+        senha += choice(caracteres)
+    return senha
+
 
 @login_required
 def index(request):
@@ -15,10 +27,13 @@ def index(request):
     pendentes = indicado.objects.filter(status__id=1)
     jovens = Usuario.objects.all()
 
+    urls = request.build_absolute_uri().find('/', 8)+1
+    final_url = request.build_absolute_uri()[:urls]
+
     if usu.tipo.nome == 'jovem' or usu.tipo.nome == 'instituiçao':
         return render(request, 'acompanhar_status.html', {'usu': usu})
     return render(request, 'index.html', {'usu': usu, 'indicados': indicados, 'fechadas': fechadas,
-                                          'pendentes': pendentes, 'jovens': jovens})
+                                          'pendentes': pendentes, 'jovens': jovens, 'final_url': final_url})
 
 
 @login_required
@@ -54,17 +69,18 @@ def form_user(request):
     form = UsuarioForm(request.POST or None, request.FILES or None)
     form2 = UserCreationForm(request.POST or None, request.FILES or None)
     usu = get_object_or_404(Usuario, user=request.user)
+    senha = gerador_senha(10)
 
     if form2.is_valid():
+        post1 = form2.save(commit=False)
+        post1.save()
         if form.is_valid():
-            post1 = form2.save(commit=False)
-            post1.first_name = form.cleaned_data['nome']
-            post1.save()
             post = form.save(commit=False)
             post.user = form2.cleaned_data['username']
             post.save()
             return redirect('usuario_list')
-    return render(request, 'form_user.html', {'form': form, 'form2': form2, 'usu': usu})
+
+    return render(request, 'form_user.html', {'form': form, 'form2': form2, 'usu': usu, 'senha': senha})
 
 
 @login_required
@@ -88,6 +104,7 @@ def indicacao(request, id):
         post1.resp_indicacao = usu
         post1.status = status
         post1.save()
+        return redirect('https://www.wassis.com.br/obrigado.html')
     return render(request, 'indicacao.html', {'form': form, 'usu': usu})
 
 
@@ -144,3 +161,19 @@ def send_email(request):
     )
 
     return redirect('instituicao_list')
+
+
+@login_required
+def form_resetpassword(request):
+    usu = get_object_or_404(Usuario, user=request.user)
+    if request.method == "POST":
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, "Your Password Changed", extra_tags='green')
+            return redirect('acompanhar_status')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    return render(request, 'form_resetpwr.html', {'form': form, 'usu': usu})
