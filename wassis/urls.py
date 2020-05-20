8c2b64089d370878bb4f1091ae5c156c0ea4b82b
unknown <williane.tads@gmail.com>
2020-05-20 18:34:57 -0300

    final setup

--- wassis/urls.py
+++ wassis/urls.py
@@ -22,7 +22,7 @@ from jovem import urls as jovem_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('indicacao/', include(jovem_url)),
+    path('', include(jovem_url)),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
